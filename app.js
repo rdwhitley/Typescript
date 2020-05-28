5734b531b5ebc288816c94c3e07e84c3b83a4430
Rafiq <raw96@icloud.com>
2020-05-28 14:49:43 -0400

    typscript variables added

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var game;
+game = "basketball";
+game = 2;
+function print(game) {
+    console.log("Billy got 20 points in his last " + game + " game");
+}
+print(game);
---
+++ app.ts
@@ -0,0 +1,22 @@
+export {};
+let game: string;
+game = "boxing";
+
+
+
+function print(game: string, points: number){
+  console.log(`Billy got 20 points in his last ${game} game`);
+}
+
+print("baseball", 23);
+let a: number = 1;
+let b: string = "string";
+let c: boolean = true;
+let d: null = null;
+let e: object = {};
+let f: undefined = undefined;
+let g: void = null;
+let h: any = "j";
+let i: any[] = ["billy",23];
+let j: Array<string> = ["23"];
+let k: Array<number> = [23, 44];

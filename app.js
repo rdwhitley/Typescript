7f8c785d74f19579ad9f6ce7516388e40f5dcd4f
Rafiq <raw96@icloud.com>
2020-05-28 15:08:58 -0400

    Arrow functions added

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 "use strict";
 exports.__esModule = true;
-var game;
-game = "basketball";
-game = 2;
-function print(game) {
-    console.log("Billy got 20 points in his last " + game + " game");
-}
-print(game);
+// let game: string;
+// game = "boxing";
+// function print(game: string, points: number){
+//   console.log(`Billy got 20 points in his last ${game} game`);
+// }
+// print("baseball", 23);
+// let a: number = 1;
+// let b: string = "string";
+// let c: boolean = true;
+// let d: null = null;
+// let e: object = {};
+// let f: undefined = undefined;
+// let g: void = null;
+// let h: any = "j";
+// let i: any[] = ["billy",23];
+// let j: Array<string> = ["23"];
+// let k: Array<number> = [23, 44];
+//Functions
+var print = function (name) { return console.log(name); };
+var double = function (number) { return number * 2; };
+print("Fiq");
+console.log(double(8));
